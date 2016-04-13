cd52bf11303dc8c7546d123fbd47abe722c64225
Harshal Varday <harshalvarday1@gmail.com>
2016-04-13 14:45:13 +0530

    Added a sample view

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET sample page. */
+router.get('/sample', function(req, res){
+    //  second parameter is JavaScript Object, which is sent to the view.
+    res.render('sample', {name:"HV", age:20, school:"SCOPE", year:2, courses:["CSE","MAT","FRE","MEE"]});
+})
+
 module.exports = router;
